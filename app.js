06f4997d3971da76f0b1d69c1a66cc066ae18727
mrkenkre <mrkenkre@gmail.com>
2024-01-30 20:00:06 -0500

    testing flow

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 
 app.use("/", healthz);
-app.use("/v2", assg);
+app.use("/v1", assg);
 
 app.listen("3000", () => {
   console.log("Server started on port 3000.");
